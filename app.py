5ea3082304c71e56ed3c509fb2dc364ccc384766
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2025-04-01 12:30:23 +0200

    patch

--- app.py
+++ app.py
@@ -36,3 +36,17 @@ def update_item(item_id: int, item: Item):
         return {"item_id": item_id, "item": item}
 
     return {"error": "Item not found"}, 404
+
+
+@app.patch("/items/{item_id}")
+def partial_update_item(item_id: int, item: Item):
+    if item_id in fake_items_db:
+        existing_item = fake_items_db[item_id]
+        existing_item.name = item.name or existing_item.name
+        existing_item.description = item.description or existing_item.description
+        existing_item.price = item.price or existing_item.price
+        existing_item.tax = item.tax or existing_item.tax
+        fake_items_db[item_id] = existing_item
+        return {"item_id": item_id, "item": existing_item}
+
+    return {"error": "Item not found"}, 404
